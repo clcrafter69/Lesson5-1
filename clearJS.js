35e19c6782ce2256a5e68f3edab83fd7e10dfd67
clcrafter69 <clcrafter@gmail.com>
2017-07-21 13:06:56 -0400

    rev 2.0

--- clearJS.js
+++ clearJS.js
@@ -1,8 +1,13 @@
 var inputText = document.getElementById("displayBox");
 var printButton = document.getElementById("print-button");
+var clearButton = document.getElementById("clear-button");
 var printArea = document.getElementById("print-area");
 
 
 printButton.addEventListener("click",function(){
     printArea.innerText += "\n" + inputText.value;
+});
+
+clearButton.addEventListener("click",function(){
+    printArea.innerHTML = " ";
 });
